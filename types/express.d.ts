d723b7d0232fe238ad96cfd18cfdb28653fc2149
chua-dao <jitae98tu@naver.com>
2024-07-17 00:03:12 +0700

    update jwtpayload

--- types/express.d.ts
+++ types/express.d.ts
@@ -2,8 +2,9 @@ import type { JwtPayload } from "jsonwebtoken";
 
 declare global {
   namespace Express {
+    // Express library
     interface Request {
       user?: string | JwtPayload; // Adjust the type based on JWT payload
-    }
+    } // define type
   }
 }
