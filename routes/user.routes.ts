9847be1d1f02643e0d39ce04c024567c75bfddf6
chua-dao <jitae98tu@naver.com>
2024-07-23 00:19:50 +0700

    update user routes

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,8 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { Router } from "express";
-import User from "../models/user.model";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import User from "../models/user.model.ts";
 
 const router = Router();
 
